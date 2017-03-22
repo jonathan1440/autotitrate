7f710a8ec42cc60ae3ba6e5bc57f4dd02058478a
sadoskik <sadoskik@gmail.com>
2017-03-22 07:46:22 -0400

    Update Results.php

--- Results.php
+++ Results.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      .words{color: #00FF00;}
-    </style>
-  </head>
-  <body>
-    <p>
-      The results should be here
-    </p>
-    <script language="php">
-    $initial = $_POST["initial"];
-    echo <p>$initial</p>;
-    </script>
-  </body>
-  </html>
+
